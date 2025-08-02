f0024f0b90236e9ecddb3d935e5f82f8afc03b83
e33io <info@e33.io>
2025-08-02 01:58:54 -0500

    Update JWM Polybar config

--- jwm/.config/polybar/launch.sh
+++ jwm/.config/polybar/launch.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
-# If your bars have ipc enabled, you can also use `polybar-msg cmd quit`
 
 # Launch Polybar, using default config location ~/.config/polybar/config
+echo "---" | tee -a /tmp/polybar.log
 polybar jwmbar 2>&1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
